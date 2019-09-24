b73847667ac804e35ddbd92cd98da3f1fd07b53d
Maggie D <45428880+maggiewld@users.noreply.github.com>
2019-09-24 10:43:42 +0100

    Remove unecessary using statements.

--- helloWorld/helloWorld/Program.cs
+++ helloWorld/helloWorld/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace helloWorld
 {
@@ -10,6 +6,7 @@ namespace helloWorld
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
