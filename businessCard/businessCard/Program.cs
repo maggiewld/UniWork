9b89ddd68b7a6918a036f1c2ef3b464032bc3c3c
Maggie D <45428880+maggiewld@users.noreply.github.com>
2019-09-24 12:05:42 +0100

    Add date of birth and age.

--- businessCard/businessCard/Program.cs
+++ businessCard/businessCard/Program.cs
@@ -12,7 +12,8 @@ namespace businessCard
         {
             //Define Variables
             string name, telNumber, employer;
-
+            int dob, age;
+       
             //Ask user for name, telephone number and employer
             Console.WriteLine("What is your name: ");
             name = Console.ReadLine();
@@ -21,8 +22,14 @@ namespace businessCard
             Console.WriteLine("Who do you work for: ");
             employer = Console.ReadLine();
 
+            //Q. How would you modify this code to also show the user's date of birth and age?
+            Console.WriteLine("What's your date of birth (DDmmYY): ");
+            dob = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How old are you: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
             //Print to console in the order: employer, name, telephone number
-            Console.WriteLine("Your employer is: " + employer + "\nYour name is: " + name + "\nYou telephone number is: " + telNumber);
+            Console.WriteLine("Your employer is: " + employer + "\nYour name is: " + name + "\nYour telephone number is: " + telNumber + "\nYour birthday is: " + dob + "\nYour age is: " + age);
             Console.ReadLine();
         }
     }
