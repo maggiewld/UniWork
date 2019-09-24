86ddd12250a5c6304793010db2a42401fd59ac19
Maggie D <45428880+maggiewld@users.noreply.github.com>
2019-09-24 16:04:30 +0100

    Add code body to Main to calc age difference and print.

--- retirementPlanner/retirementPlanner/Program.cs
+++ retirementPlanner/retirementPlanner/Program.cs
@@ -1,7 +1,8 @@
-﻿//Author: Maggie
-//SID: [REDACTED]
-//Edited: 24Sep2019
-
+﻿/*
+*Author: Maggie
+*SID: [REDACTED]
+*Edited: 24Sep2019
+*/
 using System;
 
 namespace retirementPlanner
@@ -16,7 +17,15 @@ namespace retirementPlanner
             int oldAge = 65;
             int difference;
 
+            Console.WriteLine("Please enter your age in whole years");
+            InputAge = Console.ReadLine();
 
+            //Convert console input from string to integer
+            age = Convert.ToInt32(InputAge);
+
+            difference = oldAge - age;
+            Console.WriteLine("In {0} years you will be 65 years old.", difference);
+            Console.ReadLine();
         }
     }
 }
