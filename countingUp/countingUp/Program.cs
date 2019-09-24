5d5101f6bd3e24425a88a80419e04a94c3940027
Maggie D <45428880+maggiewld@users.noreply.github.com>
2019-09-24 11:06:43 +0100

    Add program code within Main.

--- countingUp/countingUp/Program.cs
+++ countingUp/countingUp/Program.cs
@@ -10,6 +10,22 @@ namespace countingUp
     {
         static void Main(string[] args)
         {
+            //Variable to store total number
+            int total = 1;
+
+            Console.WriteLine("Total currently: {0}", total);
+            total = total + 2;
+            Console.WriteLine("Total now: {0}", total);
+            total = total + 1;
+            Console.WriteLine("Total changed to: {0}", total);
+
+            //Shorthand form of addition
+            total += 3;
+
+            Console.WriteLine("Total finally: {0}", total);
+
+            //Readline to make program pause
+            Console.ReadLine();
         }
     }
 }
