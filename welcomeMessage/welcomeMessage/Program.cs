f4fd3656c76baafcbd80419be962e2b6b0917c36
Maggie D <45428880+maggiewld@users.noreply.github.com>
2019-09-24 11:33:26 +0100

    Add code to answer commented questions to print additional messages to console.

--- welcomeMessage/welcomeMessage/Program.cs
+++ welcomeMessage/welcomeMessage/Program.cs
@@ -11,9 +11,21 @@ namespace welcomeMessage
             Console.WriteLine("Please enter your name: ");
             name = Console.ReadLine();
 
-            message = "Welcome " + name + " to " + "my program.";
+            message = "Welcome " + name + ", to " + "my program.";
             Console.WriteLine(message);
             Console.ReadLine();
+
+            //Q. How would you modify this program to also ask the user which course they were on?
+            string course;
+
+            Console.WriteLine("What course are you studying: ");
+            course = Console.ReadLine();
+
+            //Q. Having done so, how would you modify the program to print a welcome to that course?
+            message = course + "! That must be interesting!";
+            Console.WriteLine(message);
+            Console.ReadLine();
+
         }
     }
 }
