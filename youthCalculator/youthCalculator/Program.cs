394e0fefb95bd2c5c22b11491527d3c390ad7894
Maggie D <45428880+maggiewld@users.noreply.github.com>
2019-09-24 17:01:58 +0100

    Add code to request user inputs to assign to variables.

--- youthCalculator/youthCalculator/Program.cs
+++ youthCalculator/youthCalculator/Program.cs
@@ -16,9 +16,13 @@ namespace youthCalculator
             string userName;
 
             //Ask user for age in years
-            
+            Console.WriteLine("How old are you in years: ");
+            userAge = Console.ReadLine();
+
             //Ask user for name
-            
+            Console.WriteLine("What's your name: ");
+            userName = Console.ReadLine();
+
             //Display how much younger than Ian they are (Ian = 43yo)
         }
     }
