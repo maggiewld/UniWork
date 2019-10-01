89bff0893e4c40d55d5046abb4bcfec9a7052ff1
Maggie D <45428880+maggiewld@users.noreply.github.com>
2019-10-01 10:49:58 +0100

    Set up variables, request user input, initial calculation for windows.

--- paintProblem/paintProblem/Program.cs
+++ paintProblem/paintProblem/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace paintProblem
 {
@@ -10,6 +6,40 @@ namespace paintProblem
     {
         static void Main(string[] args)
         {
+            //Coverage of chosen paint
+            byte paint;
+            byte roomWidth, roomLength, roomHeight;
+            byte windowHeight, windowWidth, windowArea;
+
+
+            //Calculate room area
+                //User input
+                Console.WriteLine("Input wall length: ");
+                roomLength = Convert.ToByte(Console.ReadLine());
+
+                Console.WriteLine("Input wall width: ");
+                roomWidth = Convert.ToByte(Console.ReadLine());                
+      
+                Console.WriteLine("Input wall height: ");
+                roomHeight = Convert.ToByte(Console.ReadLine());
+
+
+            //Calculate window area
+                //User input
+                Console.WriteLine("Input window height: ");
+                windowHeight = Convert.ToByte(Console.ReadLine());
+
+                Console.WriteLine("Input window width: ");
+                windowWidth = Convert.ToByte(Console.ReadLine());
+
+                //Window area
+                windowArea = windowHeight * windowWidth
+
+            //Minus window area from room
+                //Area of paintable wall
+
+            //Calculate how much paint is required for user's room
+                
         }
     }
 }
